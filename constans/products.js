3a3dce34c096189659bd30afbeb5287005554ddc
luisga32 <luisga32@hotmail.com>
2021-05-04 10:34:15 +0200

    fix: install express for loading in heroku, feat: add price in products constants

--- constans/products.js
+++ constans/products.js
@@ -3,105 +3,134 @@ module.exports = [
         title: 'Ensalada de lentejas y queso feta',
         ingredients: 'Espinacas, rúcula, lentejas, pimiento rojo, pimiento verde, aceitunas negras, uvas y pasas',
         image: '', 
-        price: 0,
+        price: 5,
         course: 'starter'
     },
     {
         title: 'Crema de calabaza',
         ingredients: 'Calabaza, agua, patata, cebolla, aceite de girasol y sal.',
         image: '', 
-        price: 0,
+        price: 5,
         course: 'starter'
     },
     {
         title: 'Tortilla de patatas',
         ingredients: 'Patata, huevo, cebolla, aceite de oliva y sal',
         image: '', 
-        price: 0,
+        price: 5,
         course: 'starter'
     },
     {
         title: 'Ensalada mixta',
         ingredients: 'Tomate, lechuga, cebolla, atun, aceitunas negras, aceite de oliva y sal',
         image: '', 
-        price: 0,
+        price: 5,
         course: 'starter'
     },
     {
         title: 'Crema de verduras',
         ingredients: 'Cebolla, apio, agua, calabacin, zanahoria, nabo, aceite de oliva y sal.',
         image: '', 
-        price: 0,
+        price: 5,
         course: 'starter'
     },
     {
         title: 'Estofado de garbanzos',
         ingredients: 'Garbanzos, puerro, zanahoria, pimiento verde, cebolla, pimentón, caldo base, ajo, aceite de oliva virgen extra y sal ',
         image: '', 
-        price: 0,
+        price: 6,
         course: 'starter'
     },
     {
         title: 'Albondigas',
         ingredients: 'Albóndigas de cerdo y de ternera, tomate, cebolla, aceite de oliva, ajo, perejil y sal',
         image: '', 
-        price: 0,
+        price: 6,
         course: 'main'
     },
     {
         title: 'Filete de ternera con patatas',
         ingredients: 'Filete de ternera, patatas, aceite de oliva, ajo, perejil y sal',
         image: '', 
-        price: 0,
+        price: 6,
         course: 'main'
     },
     {
         title: 'Salteado de setas con arroz',
         ingredients: 'Arroz, setas, cebolla, vino, agua ,tomate, ajo, azúcar, pan, perejil, trigo, almendra, laurel, aceite y sal',
         image: '', 
-        price: 0,
+        price: 6,
         course: 'main'
     },
     {
         title: 'Bacalao con patatas y pimiento',
         ingredients: 'Bacalao, patata, aceite de girasol, pimiento verde, cebolla, aceite de girasol, harina de almendras, ajo y sal',
         image: '', 
-        price: 0,
+        price: 6,
         course: 'main'
     },
     {
         title: 'Risotto de setas',
         ingredients: 'Arroz, nata, cebolla, cepas, shiitake, seta ostra baby, namek, mantequilla, queso parmesano y caldo de pollo',
         image: '', 
-        price: 0,
+        price: 6,
         course: 'main'
     },
     {
         title: 'Manzana',
         ingredients: 'Manzana',
         image: '', 
-        price: 0,
+        price: 1,
         course: 'dessert'
     },
     {
         title: 'Pera',
         ingredients: 'Pera',
         image: '', 
-        price: 0,
+        price: 1,
         course: 'dessert'
     },
     {
         title: 'Plantano',
         ingredients: 'Plantano',
         image: '', 
-        price: 0,
+        price: 1,
         course: 'dessert'
     },
     {
         title: 'Mandarina',
         ingredients: 'Mandarina',
         image: '', 
-        price: 0,
+        price: 1,
         course: 'dessert'
     },
+    {
+        title: 'Pan',
+        ingredients: 'Pan',
+        image: '', 
+        price: 1,
+        course: 'extra'
+    },
+    {
+        title: 'Cerveza',
+        ingredients: 'Cerveza',
+        image: '', 
+        price: 1,
+        course: 'extra'
+    },
+    {
+        title: 'Agua',
+        ingredients: 'Agua',
+        image: '', 
+        price: 1,
+        course: 'extra'
+    },
+    {
+        title: 'Refresco',
+        ingredients: 'Refresco',
+        image: '', 
+        price: 1,
+        course: 'extra'
+    },
+    
 ]
